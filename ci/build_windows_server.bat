2d655c89eeb17c289c1a0f43b06968c4d90f6ce7
sandahlv <sandahl.art@gmail.com>
2026-02-05 11:09:01 -0500

    Create build_windows_server.bat

---
+++ ci/build_windows_server.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set PROJECT_PATH=UE-Pipeline-Demo\UE-Pipeline-Demo.uproject
+set BUILD_DIR=build\windows-server
+
+mkdir %BUILD_DIR%
+
+"C:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun ^
+  -project="%PROJECT_PATH%" ^
+  -noP4 -platform=Win64 ^
+  -clientconfig=Shipping -serverconfig=Shipping ^
+  -cook -allmaps -build -stage -pak -archive ^
+  -archivedirectory="%BUILD_DIR%" ^
+  -server ^
+  -prereqs
